ae490e623009bdcf77e0eecb082c631d6ae1381f
RagheedBahmad <rlb01@aub.edu.lb>
2022-12-19 21:49:49 +0200

    Battle Zone detection

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const collisionsMap = [];
 for(let i = 0; i < collisions.length; i += 160) {
     collisionsMap.push(collisions.slice(i, i + 160))
 }
+const battleZonesMap = [];
+for(let i = 0; i < battleGround.length; i += 160) {
+    battleZonesMap.push(battleGround.slice(i, i + 160))
+}
 
 const offset = {
     x: -720,
@@ -29,6 +33,21 @@ collisionsMap.forEach((row, i) => {
         }
     })
 }) // initiating collision array
+
+const battleZones = []
+battleZonesMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if(symbol !== 0) {
+            battleZones.push(new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y
+                }
+            }))
+        }
+    })
+})
+console.log(battleZones);
 
 const image = new Image();
 image.src = "Assets/Map/Pokemon World Map.png"; // Pokemon Map
@@ -64,7 +83,7 @@ const player = new Sprite({
 const background = new Sprite({position: {x: offset.x, y: offset.y}, image: image}); // Map Sprite
 const foreground = new Sprite({position: {x: offset.x, y: offset.y}, image: foreGroundImage});
 
-const movables = [background, ...boundaries, foreground]
+const movables = [background, ...boundaries, foreground, ...battleZones]
 
 let keys = new LinkedList();
 
@@ -72,7 +91,7 @@ function rectangularCollision({rectangle1, rectangle2}) {
     return (
         rectangle1.position.x + rectangle1.width * 0.8 >= rectangle2.position.x &&
         rectangle1.position.x + rectangle1.width * 0.2 <= rectangle2.position.x + rectangle2.width &&
-        rectangle1.position.y + rectangle1.height * 0.5 <= rectangle2.position.y + rectangle2.height &&
+        rectangle1.position.y + rectangle1.height * 0.6 <= rectangle2.position.y + rectangle2.height &&
         rectangle1.position.y + rectangle1.height >= rectangle2.position.y
     );
 }
@@ -85,9 +104,32 @@ function animate() {
         if(rectangularCollision({rectangle1: player, rectangle2: boundary})) {
             console.log("collision")
         }
-    })
-    player.draw();
-    foreground.draw();
+    }) //Boundaries TEMPORARY
+    battleZones.forEach(battleZone => {
+        battleZone.draw();
+    }) // Battlezones TEMPORARY
+    player.draw(); //PLAYER
+    foreground.draw(); //FOREGROUNDS
+
+    if(keys.peek() === 'w' || keys.peek() === 'a' || keys.peek() === 's' || keys.peek() === 'd') {
+        for(let i = 0; i < battleZones.length; i++) {
+            const battleZone = battleZones[i];
+            const overLappingArea =
+                (Math.min(player.position.x + player.width, battleZone.position.x + battleZone.width) -
+                Math.max(player.position.x, battleZone.position.x)) *
+                (Math.min(player.position.y + player.height, battleZone.position.y + battleZone.height) -
+                Math.max(player.position.y, battleZone.position.y));
+            if(rectangularCollision({
+                rectangle1: player,
+                rectangle2 : battleZone
+            }) && overLappingArea > (player.width * player.height) / 2
+                && Math.random() < 0.01
+            ) {
+                console.log("Battle Zone Collision")
+                break;
+            }
+        }
+    }
 
     // MOVING AND DIRECTIONAL CODE -------------------------------------------------------------------------------
 
@@ -105,17 +147,18 @@ function animate() {
                         ...boundary,
                         position: {
                             x: boundary.position.x,
-                            y: boundary.position.y + 3
+                            y: boundary.position.y + 10
                         }
                     }
                 })) {
                     moving = false;
                     break;
                 }
-                    }
+            }
+
             if(moving)
                 movables.forEach((movable) => {
-                movable.position.y += 3;
+                movable.position.y += 10;
         })
         break;
         case 'a' :
@@ -128,7 +171,7 @@ function animate() {
                     rectangle2 :{
                         ...boundary,
                         position: {
-                            x: boundary.position.x + 3,
+                            x: boundary.position.x + 10,
                             y: boundary.position.y
                         }
                     }
@@ -137,9 +180,10 @@ function animate() {
                     break;
                 }
             }
+
             if(moving)
                 movables.forEach((movable) => {
-                movable.position.x += 3;
+                movable.position.x += 10;
         })
         break;
         case 's' :
@@ -153,7 +197,7 @@ function animate() {
                         ...boundary,
                         position: {
                             x: boundary.position.x,
-                            y: boundary.position.y - 3
+                            y: boundary.position.y - 10
                         }
                     }
                 })) {
@@ -161,9 +205,10 @@ function animate() {
                     break;
                 }
             }
+
             if(moving)
                 movables.forEach((movable) => {
-                movable.position.y -= 3;
+                movable.position.y -= 10;
         })
         break;
         case 'd' :
@@ -176,7 +221,7 @@ function animate() {
                     rectangle2 :{
                         ...boundary,
                         position: {
-                            x: boundary.position.x - 3,
+                            x: boundary.position.x - 10,
                             y: boundary.position.y
                         }
                     }
@@ -185,14 +230,13 @@ function animate() {
                     break;
                 }
             }
+
             if(moving)
                 movables.forEach((movable) => {
-                movable.position.x -= 3;
+                movable.position.x -= 10;
         })
         break
     } // Player Movement and Speed
-    console.log(player.position.x);
-    console.log(player.position.y);
 }
 animate();
 
